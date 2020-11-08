d029f6a78169fc1d2739460e113a9583dc143350
Matej <matej.rabek@gmail.com>
2020-11-08 06:33:29 +0100

    first commit

---
+++ Assets/Scripts/CorutineCubeBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorutineCubeBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10;
+    [SerializeField]
+    private float rotationSpeed = 100;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 target = hit.point;
+
+                target.y = transform.position.y;
+
+                float angle = Vector3.SignedAngle(transform.forward, target, Vector3.up);
+                StartCoroutine("RotateToTarget", angle);
+                // StopCoroutine("WalkToTarget");
+                // StartCoroutine("WalkToTarget", target);
+            }
+        }
+    }
+
+    IEnumerator WalkToTarget(Vector3 target)
+    {
+        Vector3 direction = target - transform.position;
+        while (Vector3.Distance(target, transform.position) > 0.1f)
+        {
+            transform.Translate(direction.normalized * (speed * Time.deltaTime));
+            yield return null;
+        }
+    }
+
+    IEnumerator RotateToTarget(float angle)
+    {
+        while (Mathf.Abs(angle) > 1f)
+        {
+            float rotationAmount = rotationSpeed * (angle/Mathf.Abs(angle)) * Time.deltaTime;
+            transform.Rotate(transform.up, rotationAmount);
+            angle -= rotationAmount;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/CubeBehaviour.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    // Update is called once per frame
+
+    private Vector3 target;
+    private bool isTargetSet = false;
+    private bool isFacingTarget = false;
+
+    [SerializeField]
+    private float speed = 5;
+    [SerializeField]
+    private float rotationSpeed = 100;
+    void Update()
+    {
+        Debug.DrawLine(transform.position, transform.position + transform.forward, Color.red);
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.point);
+                target = hit.point;
+                target.y = transform.position.y;
+                isTargetSet = true;
+                isFacingTarget = false;
+            }
+        }
+
+        if (isTargetSet)
+        {
+            Vector3 direction =  target - transform.position;
+            Debug.DrawLine(transform.position, target);
+            if (isFacingTarget)
+            {
+                transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+                if (Vector3.Distance(target, transform.position) < 0.2f)
+                {
+                    isTargetSet = false;
+                    
+                }
+            }
+            else
+            {
+                float angle = Vector3.SignedAngle(transform.forward, direction, transform.up);
+                if ( Mathf.Abs(angle) > 1)
+                {
+                    transform.Rotate(transform.up, angle/Mathf.Abs(angle) * rotationSpeed * Time.deltaTime); 
+                }
+                else
+                {
+                    isFacingTarget = true;
+                }
+                
+            }
+        }
+    }
+}
